d2356f1b70cfd8518edc03f05cf70d8d448f3cd2
Miguel Torres <migueltm.sp@gmail.com>
2021-05-26 22:55:19 +0200

    Defined one (Customer) to many (Orders) table relationship

--- models/order.js
+++ models/order.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Order.belongsTo(models.Customer, {
+        foreignKey: 'costumerIdFK'})
     }
   };
   Order.init({
