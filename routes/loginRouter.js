5d5f6a7cdd1c09f2bed71bfa0b81e66fb6415832
ApcarJo <hansokaz@gmail.com>
2021-05-27 13:27:40 +0200

    Costumer route, controller and middlewares authenticate and admin created

---
+++ routes/loginRouter.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const loginController = require('../controllers/login.Controller');
+const pasajeroController = require('../controllers/pasajero.Controller');
+
+router.post('/', async (req, res)=> {
+    try {
+
+        const nombreCheck = req.body.nombre;
+        const passwordCheck= req.body.password;
+        let token = await loginController.validate(nombreCheck,passwordCheck);
+        let pasajero = await pasajeroController.namePassenger(nombreCheck);
+        res.status(200).json({token, pasajero});
+        
+    }catch (err) {
+        return res.status(500).json({
+            message: err.message
+        }); 
+    }
+})
+
+module.exports = router;
