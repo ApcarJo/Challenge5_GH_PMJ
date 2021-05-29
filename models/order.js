b1ebe82225c57048a3a026be0e0dcd767a6682a0
José Luis <hansokaz@gmail.com>
2021-05-29 18:04:47 +0200

    Fixed in models the foreignKey name

--- models/order.js
+++ models/order.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Order.belongsTo(models.Customer, {
-        foreignKey: 'customerIdFK'})
+        foreignKey: 'customerId'})
     }
   };
   Order.init({
