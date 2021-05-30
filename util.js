e1c3737bdb76dcefa4b834e725bda8d31973cbb1
José Luis <hansokaz@gmail.com>
2021-05-30 20:02:18 +0200

    Changed the info obtained, info of the order complete in findByCity

--- util.js
+++ util.js
@@ -22,7 +22,8 @@ class Searchcity{
         orderInfo = await Customer.findByPk(ordersFind[i].dataValues.customerId);
         
         if (orderInfo.city == city){
-            arrayCity.push(orderInfo.name, orderInfo.city, ordersFind[i].id);
+            // arrayCity.push(orderInfo.name, orderInfo.city, ordersFind[i].id); //Solo muestra la ID del order y no la información del order completa.
+            arrayCity.push(orderInfo.name, orderInfo.city, ordersFind[i]); // Muestra todo el order
             }
         }
         return arrayCity;
